50140cc7a2515431388d275bc97d073c16bbbd0b
Kelly Paxton <klypxtn@gmail.com>
2024-02-20 23:56:01 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import pigeon from './P-logo.png';
+import photo from './ship.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={pigeon} className="App-logo" alt="logo" /> <h1>igeon</h1>
+        <img src={pigeon} className="App-logo" alt="logo" /><h1>igeon</h1>
       </header>
+        <img src={photo} className='photo'/>
     </div>
   );
 }
